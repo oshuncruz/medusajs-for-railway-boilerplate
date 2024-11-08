f1dc5f26c5765d7db764f9f8cdd270717d00b30d
oshuncruz <37771084+oshuncruz@users.noreply.github.com>
2024-11-08 05:03:52 -0600

    featured products removed 4

--- medusajs-storefront/src/app/[countryCode]/(main)/page.tsx
+++ medusajs-storefront/src/app/[countryCode]/(main)/page.tsx
@@ -3,7 +3,7 @@ import { Metadata } from "next"
 
 import { getCollectionsList, getProductsList, getRegion } from "@lib/data"
 //import FeaturedProducts from "@modules/home/components/featured-products"
-// import Hero from "@modules/home/components/hero"
+import Hero from "@modules/home/components/hero"
 import { ProductCollectionWithPreviews } from "types/global"
 import { cache } from "react"
 
@@ -66,14 +66,14 @@ export default async function Home({
     return null
   }
 
-  // return (
-    // <>
-    //   <Hero />
-    //   <div className="py-12">
-    //     <ul className="flex flex-col gap-x-6">
-    //       <FeaturedProducts collections={collections} region={region} />
-    //     </ul>
-    //   </div>
-    // </>
-  // )
+  return (
+    <>
+      <Hero />
+      <div className="py-12">
+        <ul className="flex flex-col gap-x-6">
+         {/* <FeaturedProducts collections={collections} region={region} /> */}
+        </ul>
+      </div>
+    </>
+  )
 }
